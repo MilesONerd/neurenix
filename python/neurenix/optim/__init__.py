5accec7d8b6f8719ec8ecf224802b4d743f53b15
ℰ𝓃𝓏ℴ ℱ𝓊𝓀ℯ <150525657+MilesONerd@users.noreply.github.com>
2025-04-25 22:05:16 -0300

    Delete python directory

--- python/neurenix/optim/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Optimization algorithms for the Neurenix framework.
-"""
-
-from neurenix.optim.optimizer import Optimizer
-from neurenix.optim.sgd import SGD
-from neurenix.optim.adam import Adam
